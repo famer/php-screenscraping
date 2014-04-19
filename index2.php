96907125ca3e35ef74540882b8bae726e0fc9ef2
Тимур Татаршаов <famer@pksc5zaw.sin.cvut.cz>
2014-04-19 22:39:00 +0200

    DOMDocument

--- index2.php
+++ index2.php
@@ -1,13 +1,18 @@
 <?php
-//error_reporting(E_ALL);
- //ini_set("display_errors", 1);
+error_reporting(E_ALL);
+ ini_set("display_errors", 1);
 
 function getDom($path) {
-	$doc = new DOMDocument();
-	$doc->loadHTMLFile($path);
-	$xPath = new DOMXPath($doc);
-	$xPath->registerNamespace("xhtml", "http://www.w3.org/1999/xhtml");
-	return $xPath;
+  $tidy = tidy_parse_file($path, array("numeric-entities" => true, "output-xhtml" => true), 'utf8');
+  $tidy->cleanRepair();
+  $xhtml = (string) $tidy;
+$doc = new DOMDocument();
+$doc->loadHTMLFile($path);
+$xhtml =  $doc->saveHTML();
+  //$xhtml = file_get_contents($path);
+  $dom = simplexml_load_string($xhtml);
+  $dom->registerXPathNamespace("xhtml", "http://www.w3.org/1999/xhtml");
+  return $dom;
 }
 
 $timematrix = array(
@@ -28,25 +33,19 @@ $timematrix = array(
 	'20:30'
 
 	);
-function printElements($elements) {
-	if (!is_null($elements)) {
-		foreach ($elements as $element) {
-			echo  $element->nodeName. ": " . $element->nodeValue;
-		}
-	}
-}
 
 if (!$_GET['q']) {
 	echo '<form><input type="text" name="q" /><input type="submit" /></form>';
 }
+$_GET['q'] = 'valenta';
 if ($_GET['q']) {
 
 	$url = "https://usermap.cvut.cz/search/?js=true&attrs=exchPersonalId&format=HTML&query=" . $_GET['q'];
 	$Dom = getDom($url);
-	$xPath = '//table[@id="profile"]/tbody/tr';
-	$tr = $Dom->query($xPath);
+	$xPath = '//xhtml:*[@id="profile"]/xhtml:tbody/xhtml:tr';
+	$tr = $Dom->xpath($xPath);
+	echo $tr;
 	print_r($tr);
-	printElements($tr);
 }
 exit;
 
@@ -54,10 +53,10 @@ $url = 'https://usermap.cvut.cz/profile/valenta/';
 $url = 'profile.html';//'https://usermap.cvut.cz/profile/'.?_GET['username'];
 
 $Dom = getDom($url);
-$phone  = $Dom->xpath('//div[@id="prf"]/table[1]/tbody/tr[3]/td[2]');
-$room = $Dom->xpath('//div[@id="prf"]/table[1]/tbody/tr[2]/td[2]/a');
-$number = $Dom->xpath('//div[@id="prf"]/table[2]/tr[2]/td[2]');
-$email = $Dom->xpath('//div[@id="prf"]/table[1]/tbody/tr[5]/td[2]/a[1]');
+$phone  = $Dom->xpath('//xhtml:div[@id="prf"]/xhtml:table[1]/xhtml:tbody/xhtml:tr[3]/xhtml:td[2]');
+$room = $Dom->xpath('//xhtml:div[@id="prf"]/xhtml:table[1]/xhtml:tbody/xhtml:tr[2]/xhtml:td[2]/xhtml:a');
+$number = $Dom->xpath('//xhtml:div[@id="prf"]/xhtml:table[2]/xhtml:tr[2]/xhtml:td[2]');
+$email = $Dom->xpath('//xhtml:div[@id="prf"]/xhtml:table[1]/xhtml:tbody/xhtml:tr[5]/xhtml:td[2]/xhtml:a[1]');
 //print_r( $phone);
 //print_r( $numb);
 echo 'Contacts:', "\n";
@@ -74,7 +73,7 @@ substr($number, 2, 2).'/u'.$number.'000.html';
 
 $url = 'schedule.html';
 $Dom = getDom($url);
-$rooms = $Dom->xpath('//div[@id="content"]/table/tbody/tr[3]/td');
+$rooms = $Dom->xpath('//xhtml:div[@id="content"]/xhtml:table/xhtml:tbody/xhtml:tr[3]/xhtml:td');
 echo 'Today at:', "\n";
 
 $colspanSum = 0;
